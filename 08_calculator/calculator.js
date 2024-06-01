e31d6751d69000bd7c04b32905d8eb0419ce764e
mr-r <rbbagyei@gmail.com>
2024-06-01 11:28:25 +0000

    calculator exercise done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total + num,0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	for(let i = num; i > 0; i --) {
+    total = total * i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
