9264418bda9a4268e3df6403408479d3a0944888
mr-r <rbbagyei@gmail.com>
2024-06-01 12:34:55 +0000

    palindrome exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const palindrome = str.toLowerCase().split('').filter((text) => alphanumerical.includes(text)).join('');
+  const reversePalindrome = palindrome.split('').reverse().join('')
+  return palindrome === reversePalindrome; 
 };
 
 // Do not edit below this line
